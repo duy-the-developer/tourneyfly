9389f8d4bfb5c59d7e0996574c93b0842945e844
duy-the-developer <duythedeveloper@gmail.com>
2022-11-27 05:40:07 -0500

    finalize aside

--- components/Headlines/index.tsx
+++ components/Headlines/index.tsx
@@ -1,4 +1,5 @@
 import { parseISO, format } from 'date-fns'
+import Link from 'next/link'
 
 import type { TArticle } from '../../types/TArticle'
 const Headlines = ({ articles }: { articles: TArticle[] }) => {
@@ -8,18 +9,24 @@ const Headlines = ({ articles }: { articles: TArticle[] }) => {
         const { publishedAt, title, url, urlToImage } = each
 
         return (
-          <li key={url} className='flex items-center space-x-6 px-4 py-4'>
-            <img
-              src={urlToImage}
-              alt={title}
-              className='h-24 w-24 flex-none rounded-md bg-gray-100 object-cover object-center'
-            />
-            <div className='flex-col space-y-1'>
-              <p className='text-aqua line-clamp-2'>{title}</p>
-              <p className='text-sm flex-none font-medium text-gray-200'>
-                {format(parseISO(publishedAt), 'PPpp')}
-              </p>
-            </div>
+          <li key={url}>
+            <Link
+              href={url}
+              className='flex items-center space-x-6 px-4 py-4 cursor-pointer hover:bg-slate-800 rounded-lg group'
+              target='_blank'
+            >
+              <img
+                src={urlToImage}
+                alt={title}
+                className='h-20 w-20 flex-none rounded-md bg-gray-100 object-cover object-center'
+              />
+              <div className='flex-col space-y-1'>
+                <p className='text-aqua line-clamp-2'>{title}</p>
+                <p className='text-sm flex-none font-medium text-gray-200'>
+                  {format(parseISO(publishedAt), 'PPpp')}
+                </p>
+              </div>
+            </Link>
           </li>
         )
       })}
