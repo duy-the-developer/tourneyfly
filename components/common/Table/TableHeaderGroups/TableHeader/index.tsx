19eb5ea0ea992ace456a5b453ae44620f6d02144
duy-the-developer <duythedeveloper@gmail.com>
2022-12-06 15:08:35 -0500

    table header refactor

--- components/common/Table/TableHeaderGroups/TableHeader/index.tsx
+++ components/common/Table/TableHeaderGroups/TableHeader/index.tsx
@@ -3,35 +3,34 @@ import classNames from '../../../../../utils/classNames'
 import { flexRender } from '@tanstack/react-table'
 import type { Header } from '@tanstack/react-table'
 
-export const TableHeader = ({
-  header,
-  i,
-}: {
-  header: Header<any, unknown>
-  i: number
-}) => {
-  return (
-    <th
-      className={`${classNames(
-        i === 0 ? 'text-left' : 'text-center',
-        header.column.getCanSort() ? 'cursor-pointer select-none' : ''
-      )} transition py-3.5 px-3 font-semibold h-8 hover:bg-purple`}
-      onClick={header.column.getToggleSortingHandler()}
-    >
-      {header.isPlaceholder ? null : (
-        <div
-          className={classNames(
+type TProps = {
+    header: Header<any, unknown>
+    i: number
+}
+
+const HeaderContent = ({ header, i }: TProps) => (
+    <div
+        className={classNames(
             i === 0 ? 'justify-start' : 'justify-center',
             'flex items-center'
-          )}
-        >
-          {flexRender(header.column.columnDef.header, header.getContext())}
-          {{
+        )}
+    >
+        {flexRender(header.column.columnDef.header, header.getContext())}
+        {{
             asc: <ChevronUpIcon className='w-5 h-5 ml-1' />,
             desc: <ChevronDownIcon className='w-5 h-5 ml-1' />,
-          }[header.column.getIsSorted() as string] ?? null}
-        </div>
-      )}
+        }[header.column.getIsSorted() as string] ?? null}
+    </div>
+)
+
+export const TableHeader = ({ header, i }: TProps) => (
+    <th
+        className={`${classNames(
+            i === 0 ? 'text-left' : 'text-center',
+            header.column.getCanSort() ? 'cursor-pointer select-none' : ''
+        )} transition py-3.5 px-3 font-semibold h-8 hover:bg-purple`}
+        onClick={header.column.getToggleSortingHandler()}
+    >
+        {header.isPlaceholder ? null : <HeaderContent header={header} i={i} />}
     </th>
-  )
-}
+)
--- components/common/Table/TableHeaderGroups/index.tsx
+++ components/common/Table/TableHeaderGroups/index.tsx
@@ -1,14 +1,22 @@
 import { TableHeader } from './TableHeader'
 import type { CoreHeaderGroup, Table } from '@tanstack/react-table'
 
-export const TableHeaderGroups = ({ table }: { table: Table<any> }) => {
-    const createHeaderGroup = (headerGroup: CoreHeaderGroup<any>) => (
+const HeaderRow = ({ headerGroup }: { headerGroup: CoreHeaderGroup<any> }) => {
+    const headers = headerGroup.headers.map((header, i) => (
+        <TableHeader header={header} i={i} key={header.id} />
+    ))
+
+    return (
         <tr key={headerGroup.id} className='text-aqua text-sm leading-8'>
-            {headerGroup.headers.map((header, i) => (
-                <TableHeader header={header} i={i} key={header.id} />
-            ))}
+            {headers}
         </tr>
     )
-
-    return <thead>{table.getHeaderGroups().map(createHeaderGroup)}</thead>
 }
+
+export const TableHeaderGroups = ({ table }: { table: Table<any> }) => (
+    <thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+            <HeaderRow headerGroup={headerGroup} />
+        ))}
+    </thead>
+)
