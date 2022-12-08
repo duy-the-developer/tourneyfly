77a2eb03d2706bb22a184031fc8ce697ffce0d66
duy-the-developer <duythedeveloper@gmail.com>
2022-12-08 07:08:51 -0500

    lint attempt

--- components/common/ComboBoxWithImage/index.tsx
+++ components/common/ComboBoxWithImage/index.tsx
@@ -6,7 +6,7 @@ import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { countries } from '../../../data.test'
 import Image from 'next/image'
 
-import type { TCountry } from '../../AddTeamButton'
+import type { TCountry } from '../../../types'
 
 type TProps = {
     label: string
@@ -35,8 +35,10 @@ export const ComboBoxWithImage = ({ label, selected, setSelected }: TProps) => {
                 <Combobox.Input
                     className='text-slate-400 w-full rounded-md border border-gray-300 bg-slate-800 py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm'
                     onChange={(event) => setQuery(event.target.value)}
-                    // @ts-ignore
-                    displayValue={(country) => country?.name}
+                    displayValue={(country) => {
+                        // @ts-ignore
+                        return (country as TCountry)!.name
+                    }}
                 />
                 <Combobox.Button className='absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none'>
                     <ChevronUpDownIcon
